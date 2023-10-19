9b0320c62943cc17aa172fd01448e9193394b58b
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-10-19 16:08:11 +0300

    Create class Phone

---
+++ src/main/java/com/example/app/SpringBootWebClients/entity/Phone.java
@@ -0,0 +1,11 @@
+package com.example.app.SpringBootWebClients.entity;
+
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+    private int id;
+    private String img;
+}
