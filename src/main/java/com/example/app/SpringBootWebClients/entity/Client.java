665f69353a14f9984bfd9f74aa77962bf364fea8
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-10-19 16:07:56 +0300

    Create class Client

---
+++ src/main/java/com/example/app/SpringBootWebClients/entity/Client.java
@@ -0,0 +1,22 @@
+package com.example.app.SpringBootWebClients.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "clients")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+    @Column(name = "first_name")
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    @Column(name = "phone")
+    private String phone;
+}
